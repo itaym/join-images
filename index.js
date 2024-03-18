62cbaaa70fdab5874999c0215fec638cc34679f9
Itay Merchav <itaymer@gmail.com>
2024-03-18 12:06:35 +0200

    Did it just for fun

--- index.js
+++ index.js
@@ -3,34 +3,28 @@ const  { selectTwoNoisyRows } = require('./noise.js')
 const  { image1, image2, resultImage } = require('./mock.js')
 const  { compareTwoArrays } = require('./compareTwoArrays.js')
 
-const img1 = image1
-const img2 = image2
+;((img1, img2) => {
 
-const candidates = createCandidates(img1, img2, selectTwoNoisyRows(img2))
+    const candidates = createCandidates(img1, img2, selectTwoNoisyRows(img2))
 
-let foundAt = img1.length
+    let foundAt = img1.length
 
-for (let candidate of candidates) {
-    if (checkCandidate(candidate, img1, img2)) {
-        foundAt = candidate
-        break
-    }
-}
-img1.splice(foundAt, Infinity )
-const theResult = [img1, img2].flat()
-
-try {
-    if (theResult.length !== resultImage.length)
-        throw new Error('Result length is wrong')
-
-    for (let x = 0; x < theResult.length; x++) {
-        if (!compareTwoArrays(theResult[x], resultImage[x])) {
-            throw new Error(`Error in RESULT at row ${x}!`)
+    for (let candidate of candidates) {
+        if (checkCandidate(candidate, img1, img2)) {
+            foundAt = candidate
+            break
         }
     }
-    console.log('All went fine')
-}
-catch (e) {
-    console.error(e.message)
-}
+    img1.splice(foundAt, Infinity)
+    const theResult = [img1, img2].flat()
+
+    console.assert(theResult.length === resultImage.length, 'Result length is wrong')
+
+    for (let x = 0; x < theResult.length; x++) {
+        console.assert(compareTwoArrays(theResult[x], resultImage[x]), `Error in RESULT at row ${x}!`)
+    }
+})(image1, image2)
+
+console.log('DONE!!!')
+
 
